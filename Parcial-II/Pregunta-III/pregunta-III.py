c9e0281f598edb9378ebb40585db0d3d88d456fb
valeria <16-11233@usb.ve>
2023-11-16 13:09:10 -0400

    3.b

--- Parcial-II/Pregunta-III/pregunta-III.py
+++ Parcial-II/Pregunta-III/pregunta-III.py
@@ -1,10 +1,31 @@
-X = 0
-Y = 0
-Z = 0
+from itertools import combinations
 
-a = 2 * X + 3 * Y + 2
-b = 4 * Y + 5 * Z + 1
-c = 5 * X + 2 * Z + 3
-d = (a + b + c) % 7
+def sublistas_crecientes(lista):
+    print("[]")
+    for i in lista:
+        for sublista in combinations(lista, i): # Todas las posibles combinaciones de i nro de elementos de la lista
+            if list(sublista) == sorted(sublista): # Ve si la sublista está ordenada
+                yield list(sublista)
 
-print(a, b, c, d)
+def hallar_sublistas_crecientes(lista):
+    for sublist in sublistas_crecientes(lista):
+        print(sublist)
+
+def main():
+    lista = [1, 4, 3, 2, 5]
+    lista1 = [5, 4, 3, 2, 1]
+    lista2 = []
+
+    print(f'lista: {lista}')
+    hallar_sublistas_crecientes(lista)
+    print("\n")
+
+    print(f'lista1: {lista1}')
+    hallar_sublistas_crecientes(lista1)
+    print("\n")
+
+    print(f'lista2: {lista2}')
+    hallar_sublistas_crecientes(lista2)
+
+if __name__ == "__main__":
+    main()
